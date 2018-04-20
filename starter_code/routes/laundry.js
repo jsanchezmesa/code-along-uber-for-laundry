1451d41cb1e731f274aa32c7cbe0b403b82747e8
Juan Sanchez Mesa <sanchezmesaj@gmail.com>
2018-04-20 12:32:03 +0200

    Iteration 4 - Become a launderer

--- starter_code/routes/laundry.js
+++ starter_code/routes/laundry.js
@@ -1,8 +1,26 @@
 const express = require("express");
 const router = express.Router();
+const User = require("../models/user");
+const ensureLoggedIn = require("../middlewares/ensureLoggedIn");
 
-router.get("/dashboard", (req, res, next) => {
-  res.render("laundry/dashboard");
+router.get("/dashboard", ensureLoggedIn("/login"), (req, res, next) => {
+  res.render("laundry/dashboard", {user: req.user});
+})
+
+router.post("/launderers", ensureLoggedIn("/login"), (req, res, next) => {
+  const id = req.user.id;
+
+  const laundererInfo = {
+    fee: req.body.fee,
+    isLaunderer: true
+  };
+
+  User.findByIdAndUpdate( id, laundererInfo)
+  .then( () => res.redirect("/dashboard") )
+  .catch( err => {
+    console.log(err);
+    next(err); 
+  } );
 })
 
 module.exports = router;
